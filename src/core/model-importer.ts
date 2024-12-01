48f6378c0732a68074624b6030bde8b263ae33ce
Milton Black <arziblack2@gmail.com>
2024-12-01 17:34:49 +0100

    feat: setup the model importer imports

---
+++ src/core/model-importer.ts
@@ -0,0 +1,5 @@
+import * as THREE from "three";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import * as xml2js from "xml2js";
